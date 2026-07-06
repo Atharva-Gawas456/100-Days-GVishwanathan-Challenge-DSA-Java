cf3badb9de4c10c9b888238270e2be29156081f8
Atharva-Gawas456 <atharva.gawas182@gmail.com>
2026-07-06 23:19:49 +0530

    Some Codeforces Problems

---
+++ LeaderArray.java
@@ -0,0 +1,9 @@
+public class LeaderArray {
+    public static void main(String[] args) {
+        int[] arr = new int[0];
+
+        for(int i = 0; true; i++){
+            arr[i] = i;
+        }
+    }
+}
